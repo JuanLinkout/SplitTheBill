96894defde5721548a7e9d0d9c4767139a49707b
Juan Lucas Rossi <juan.lucas.rossi@gmail.com>
2023-10-24 20:02:28 -0300

    feature: inicializa classes das telas que o app irá possuir

---
+++ app/src/main/java/com/example/splitthebill/presentation/customerdetails/CustomerDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.splitthebill.presentation.customerdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.splitthebill.R
+
+class CustomerDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_details)
+    }
+}
---
+++ app/src/main/java/com/example/splitthebill/presentation/orderdetails/OrderDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.splitthebill.presentation.orderdetails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.splitthebill.R
+
+class OrderDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_details)
+    }
+}
