af43398e2c612f93f27e71f58ca08543cd24e33b
Juan Lucas Rossi <juan.lucas.rossi@gmail.com>
2023-10-24 19:58:45 -0300

    chore: inicializa o projeto e configurações iniciais

---
+++ app/src/main/java/com/example/splitthebill/presentation/bills/BillsScreen.kt
@@ -0,0 +1,12 @@
+package com.example.splitthebill.presentation.bills
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.splitthebill.R
+
+class BillsScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bills_screen)
+    }
+}
