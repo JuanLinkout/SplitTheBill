7426c021f0cf43b8852ea70c86d40ed4e86b8037
Juan Lucas Rossi <juan.lucas.rossi@gmail.com>
2023-10-25 21:55:14 -0300

    refactor: altera nome de orderDetails para orderItemDetails

--- app/src/main/java/com/example/splitthebill/presentation/orderitemdetails/OrderItemDetailsActivity.kt
+++ app/src/main/java/com/example/splitthebill/presentation/orderitemdetails/OrderItemDetailsActivity.kt
@@ -1,10 +1,10 @@
-package com.example.splitthebill.presentation.orderdetails
+package com.example.splitthebill.presentation.orderitemdetails
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.splitthebill.R
 
-class OrderDetailsActivity : AppCompatActivity() {
+class OrderItemDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_details)
