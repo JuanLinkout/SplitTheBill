cccec55d759e945e9c222fb2bfe64cdceea857f8
Juan Lucas Rossi <juan.lucas.rossi@gmail.com>
2023-10-27 19:52:45 -0300

    feature: altera CustomersBills para fragment

--- app/src/main/java/com/example/splitthebill/presentation/customers/CustomersBillsFragment.kt
+++ app/src/main/java/com/example/splitthebill/presentation/customers/CustomersBillsFragment.kt
@@ -1,23 +1,28 @@
 package com.example.splitthebill.presentation.customers
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.splitthebill.databinding.ActivityCustomersScreenBinding
+import com.example.splitthebill.R
+import com.example.splitthebill.databinding.FragmentCustomersBillsBinding
 import com.example.splitthebill.domain.entities.customers.CustomerBill
 import com.example.splitthebill.presentation.adapters.CustomerBillAdapter
 import com.example.splitthebill.presentation.utils.StatusBarUtil
 
-class CustomersBillsActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityCustomersScreenBinding
+class CustomersBillsFragment : Fragment() {
+    private lateinit var binding: FragmentCustomersBillsBinding
     private lateinit var viewModel: CustomersBillsViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityCustomersScreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentCustomersBillsBinding.inflate(layoutInflater)
 
         viewModel = ViewModelProvider(
             this,
@@ -32,5 +37,7 @@ class CustomersBillsActivity : AppCompatActivity() {
         val statusBarHeight = StatusBarUtil.getStatusBarHeight(binding.root.context)
         binding.customerBillRecyclerView.setPadding(16, 16 + statusBarHeight, 16, 16)
         binding.customerBillRecyclerView.layoutManager = LinearLayoutManager(binding.root.context)
+
+        return binding.root
     }
 }
